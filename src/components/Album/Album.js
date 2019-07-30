a9d777585e68089870989f9a679e7df8f198ed74
DannnYX <icandos@mail.ru>
2019-07-31 00:00:28 +0300

    contacts done

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Album.css'
+import {Photo} from "react-photo-gallery";
 import Gallery from "react-photo-gallery";
+import LazyLoad from 'react-lazyload';
+
+
+const renderLazy = (props) => <LazyLoad placeholder={<div>dddd</div>}>{Photo(props)}</LazyLoad>;
 
 class Album extends React.Component {
   render() {
-    console.log(<Gallery photos={this.props.photos}/>);
-    return (<Gallery photos={this.props.photos} limitNodeSearch={3}/>)
+    return <Gallery photos={this.props.photos}
+                    limitNodeSearch={3}
+                    renderImage={renderLazy}/>;
   }
 }
 
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,22 +1,77 @@
 import React from 'react';
+import './Contacts.css'
 
 class Contacts extends React.Component {
-    componentDidMount() {
-      /*
-        axios.get('/wedding-albums')
-            .then(response => {
-                const images = response.data;
-                //console.log(images);
-                this.setState({
-                    images: images
-                })
-            });
-       */
-    }
+  render() {
+    return (
+        <div className="section">
+          <div className="contacts">
+            <section className="contacts-desc">
 
-    render() {
-      return ("Contacts");
-    }
+
+              <p>27.04−24.05 Italy / Италия<br/>25.05−29.05 Germany / Германия</p>
+              <p><br/></p>
+              <p>для быстрой связи пишите в&nbsp;
+                <b>
+                  <a href="https://www.instagram.com/ruslan_polyakov_photo/" target="_blank">
+                    <u>
+                      <i>DirectInsta</i>
+                    </u>
+                  </a>
+                </b>&nbsp;или&nbsp;
+                <a href="tel:+79649111150" target="_blank">
+                  <u><b><i>WhatsApp</i></b></u>
+                </a>
+              </p><p>Снимаю в&nbsp;России и&nbsp;Европе.<br/>
+            </p>
+              <h3><b>Мои контакты</b></h3>
+              <p>
+                <a href="https://vk.com/ruslan_polyakov" target="_blank">Вконтакте</a><br/>
+                <a href="https://www.facebook.com/polyakovfoto" target="_blank">Facebook</a><br/>
+                <a href="https://www.instagram.com/ruslan_polyakov_photo/" target="_blank">Insta</a>
+              </p>
+              <p>
+                <a href="https://t.me/ruslan_polyakov" target="_blank">Telegram</a>
+                <br/>Mob.:&nbsp;
+                <a href="tel:+79649111150" target="_blank">+7 (964) 911-11-50</a>
+                &nbsp;
+              </p>
+              <p>
+                <a href="http://mailto:ruslan.polyakov.photo@gmail.com"
+                   target="_blank">ruslan.polyakov.photo@gmail.com
+                </a>
+              </p>
+
+
+            </section>
+            <section className="round-image">
+
+              <a href="https://www.instagram.com/ruslan_polyakov_photo/" target="_blank">
+                <div className="lazy-image js-lazy-image  -loaded"
+                     data-width="1424"
+                     data-height="1424"
+                     data-aspect="1.0">
+                  <canvas className="placeholder"
+                          width="1424"
+                          height="1424"
+                          style={{backgroundColor: 322821}}>
+                  </canvas>
+                  <img alt="Контакты фотографа  Руслан Поляков — Фотограф Краснодар Италия"
+                       className="person-img" data-sizes="auto"
+                       data-src="https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518c002080012036a7067.jpg?auth=3f2b255fb5dc28d45dc7b323e8c0f28fc7bf8c27e65f9a581d6c7c297408b3b0"
+                       data-srcset="https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518c002080012036a7067.jpg?auth=3f2b255fb5dc28d45dc7b323e8c0f28fc7bf8c27e65f9a581d6c7c297408b3b0 320w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518e003080012036a7067.jpg?auth=ecb574e81955b5e7fb1b8fd4aa21df3ebcce4182d62487c62f88c5af2d7a22f1 480w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a05188005080012036a7067.jpg?auth=be1699aaadad462d1fd95b8e89f200011edeb7ef20cb37b75489982f80571f4b 640w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518c007080012036a7067.jpg?auth=0aae16dfa19750eef8e6ac321428d54c4842279f898bb6daaf494e3e63eee947 960w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518800a080012036a7067.jpg?auth=37cdeb3397d4936888d8c07e321f946e6e9fae5f6fc63748e04f5f3c2aa1980a 1280w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518900b080012036a7067.jpg?auth=958b4b020b39912d89a981602feef0e171123858449d8b48713aba495350afbf 1424w"
+                       sizes="553px"
+                       srcSet="https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518c002080012036a7067.jpg?auth=3f2b255fb5dc28d45dc7b323e8c0f28fc7bf8c27e65f9a581d6c7c297408b3b0 320w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518e003080012036a7067.jpg?auth=ecb574e81955b5e7fb1b8fd4aa21df3ebcce4182d62487c62f88c5af2d7a22f1 480w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a05188005080012036a7067.jpg?auth=be1699aaadad462d1fd95b8e89f200011edeb7ef20cb37b75489982f80571f4b 640w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518c007080012036a7067.jpg?auth=0aae16dfa19750eef8e6ac321428d54c4842279f898bb6daaf494e3e63eee947 960w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518800a080012036a7067.jpg?auth=37cdeb3397d4936888d8c07e321f946e6e9fae5f6fc63748e04f5f3c2aa1980a 1280w, https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518900b080012036a7067.jpg?auth=958b4b020b39912d89a981602feef0e171123858449d8b48713aba495350afbf 1424w"
+                       src="https://img.wfolio.ru/wfolio/sites/670/assets/1538042773_b36c22/0a0518801408010a0d18900b10900b200028f00208040a0518c002080012036a7067.jpg?auth=3f2b255fb5dc28d45dc7b323e8c0f28fc7bf8c27e65f9a581d6c7c297408b3b0"/>
+                </div>
+              </a>
+
+
+            </section>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default Contacts;
