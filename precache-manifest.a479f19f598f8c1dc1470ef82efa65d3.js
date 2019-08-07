50b5cd801d30aaa64df83d14854c07e4f6d03421
DannnYX <icandos@mail.ru>
2019-08-07 10:51:56 +0300

    Updates

---
+++ precache-manifest.a479f19f598f8c1dc1470ef82efa65d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40d3a51e8b2270bc438ca01f2a552df",
+    "url": "/WFolio/index.html"
+  },
+  {
+    "revision": "f3713aa77c5cfad44c7e",
+    "url": "/WFolio/static/css/main.41287231.chunk.css"
+  },
+  {
+    "revision": "ce96867c6002f9dbd3fc",
+    "url": "/WFolio/static/js/2.30caae3b.chunk.js"
+  },
+  {
+    "revision": "f3713aa77c5cfad44c7e",
+    "url": "/WFolio/static/js/main.27ad7384.chunk.js"
+  },
+  {
+    "revision": "352e1fd89746f5e07b34",
+    "url": "/WFolio/static/js/runtime~main.b445d94f.js"
+  },
+  {
+    "revision": "bf6a7a1816dfd65c864441e76dd19767",
+    "url": "/WFolio/static/media/logo.bf6a7a18.png"
+  }
+]);
